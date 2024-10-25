23f2d1c26406fb2918e2152c2d1edb70037b4efc
Brandon Shihabi <brandon.shihabi@gmail.com>
2024-10-25 01:54:14 -0700

    Added ability to fetch and update bio on the server

--- packages/backend/src/database/entities.ts
+++ packages/backend/src/database/entities.ts
@@ -1,5 +1,21 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany, Generated, Tree, TreeChildren, TreeParent, ManyToMany, JoinColumn, JoinTable, CreateDateColumn, UpdateDateColumn } from "typeorm"
 
+export enum Gender {
+	kMale      = "Male",
+	kFemale    = "Female",
+	kNonBinary = "Non-Binary",
+	kOther     = "Other",
+}
+
+export enum SexualOrientation {
+	kStraight  = "Straight",
+	kGay       = "Gay",
+	kLesbian   = "Lesbian",
+	kBisexual  = "Bisexual",
+	kAsexual   = "Asexual",
+	kOther     = "Other",
+}
+
 @Entity("user")
 export class User {
 	@PrimaryGeneratedColumn({ name: "ROWID" })
@@ -14,4 +30,25 @@ export class User {
 
 	@Column({ type: "text" })
 	readonly password: string;
+
+	@Column({ type: "longtext" })
+	bio: string;
+
+	@Column({ type: "enum", enum: Gender })
+	gender: Gender;
+
+	@Column({ type: "text" })
+	pronouns: string;
+
+	@Column({ type: "enum", enum: SexualOrientation })
+	sexual_orientation: SexualOrientation;
+
+	@Column({ type: "int" })
+	birthday_ms_since_epoch: number;
+
+	@Column({ type: "int" })
+	height_mm: number;
+
+	@Column({ type: "text" })
+	occupation: string;
 }
