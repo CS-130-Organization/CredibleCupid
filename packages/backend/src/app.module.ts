15505b1d220df2c4d8eb13e69e75df7fbd178bc8
Brandon Shihabi <brandon.shihabi@gmail.com>
2024-11-05 16:44:48 -0800

    Prevent database error from showing up on backend container startup

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -5,14 +5,14 @@ import { TypeOrmModule } from "@nestjs/typeorm"
 import { User } from "./database/entities"
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { DATABASE_CONTAINER, DATABASE_PASSWORD, DATABASE_NAME } from "./env"
+import { DATABASE_PASSWORD, DATABASE_NAME } from "./env"
 
 
 @Module({
 	imports: [
 		TypeOrmModule.forRoot({
 			type: 'mysql',
-			host: DATABASE_CONTAINER,
+			host: 'db',
 			port: 3306,
 			username: 'root',
 			password: DATABASE_PASSWORD,
