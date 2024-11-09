b274da689daeaf31f15cb62a4d5f3d8ba6377e5c
Brandon Shihabi <brandon.shihabi@gmail.com>
2024-11-09 12:22:50 -0800

    Added referral system with verification

--- packages/backend/src/database/entities.ts
+++ packages/backend/src/database/entities.ts
@@ -37,7 +37,7 @@ export class User {
 	@Column({ type: "text", nullable: true })
 	last_name: string | undefined;
 
-	@Column({ type: "text", nullable: true })
+	@Column({ type: "longtext", nullable: true })
 	bio: string | undefined;
 
 	@Column({ type: "enum", enum: Gender, default: Gender.kOther })
@@ -68,4 +68,28 @@ export class User {
 
 	@Column({ type: "text", nullable: true })
 	profile_pic: string | undefined;
+
+	@OneToMany(() => Referral, (referral) => referral.user)
+	@JoinTable()
+	referrals: Referral[];
+}
+
+@Entity("referral")
+export class Referral {
+	@PrimaryGeneratedColumn({ name: "ROWID" })
+	readonly ROWID: number;
+
+	@Column({ type: "text" })
+	@Generated("uuid")
+	readonly guid: string;
+
+	@Column({ type: "longtext" })
+	email: string;
+
+	@Column({ type: "longtext" })
+	message: string;
+
+	@ManyToOne(() => User, (user) => user.referrals)
+	@JoinTable()
+	user: User;
 }
